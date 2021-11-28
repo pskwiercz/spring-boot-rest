bd512416f81e3de86f820397762751a1da925d46
pskwiercz <przemek.skwiercz@gmail.com>
2021-11-28 21:25:55 +0100

    Add exceptions handling

--- src/main/java/com/pskwiercz/springbootrest/exception/ResponseException.java
+++ src/main/java/com/pskwiercz/springbootrest/exception/ResponseException.java
@@ -1,6 +1,32 @@
 package com.pskwiercz.springbootrest.exception;
 
+import java.util.Date;
+
 public class ResponseException extends RuntimeException {
+    private Date timestamp;
     private String msg;
     private String details;
+
+    public ResponseException(Date timestamp, String msg, String details) {
+        this.timestamp = timestamp;
+        this.msg = msg;
+        this.details = details;
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
